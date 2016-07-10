6fc406d197794d1e0de8714971bd4fd42829b78a
ohnarya <ohnarya@tamu.edu>
2016-07-10 00:53:31 +0000

    add more information

--- frontend/views/parkinglot/view.php
+++ frontend/views/parkinglot/view.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use kartik\widgets\ActiveForm;
 use kartik\widgets\SwitchInput;
-
+use yii\bootstrap\Modal;
 
 $this->title = 'Parking Lot Information';
 ?>
@@ -107,15 +107,33 @@ $form = ActiveForm::begin([
     </div>   
     <br><br>
     <div class='row'>
+        <div class='col-md-12'>
+<?php
+Modal::begin([
+    'header' => '<h4>Parking Lot Rules and Regulations</h4>',
+    'headerOptions'=>['class'=>'bg-primary'],
+    'closeButton'=>[],
+    'toggleButton' => ['label' => '<i class="fa fa-car" aria-hidden="true"></i> parking rules','class'=>'btn-xs btn-danger'],
+    'footer'=> Html::a('close')
+]);
+?>
+
+&bull;A user can use a parking lot which the use has the permit of anytime.<br>
+&bull;Some parking lots are open to the public at night (17:00~8:00 next day).<br>
+&bull;Some parking lots are open to the public during Summer (June,July,and August).<br>
+&bull;Some parking lots are closed when the school has home-games.<br>
+&nbsp;- Even to the permit holders.<br>
+&bull;Some parking lots are close due to the construction.<br>
+
+<?php
+
+Modal::end();           
+?>             
+        </div>
+    </div>
+    <div class='row'>
         <div class="col-md-12">
 <pre class="site-helper">
-&bull; Parking Lot Rules and Regulations :
-  A user can use a parking lot which the use has the permit of anytime.
-  Some parking lots are open to the public at night (17:00~8:00 next day).
-  Some parking lots are open to the public during Summer (June,July,and August).
-  Some parking lots are closed when the school has home-games even to the permit holders.
-  Some parking lots are close due to the construction.
-  
 &bull; Parking Lot Status :    
   <span class='hightlighted-word'>Night</span> : available parking lots from 17:00 to 8:00 next day.
   <span class='hightlighted-word'>Summer</span> : availiable parking lots in June, July, and August.
@@ -126,7 +144,7 @@ $form = ActiveForm::begin([
   <span class='hightlighted-word'>Easyparking</span> : a parking lot is easy to park.
   <span class='hightlighted-word'>Easyexit</span> : a parking lot is easy to exit.
     
-&bull; When a location is clicked on the map, its <strong>address</strong> will be automatically filled.
+&bull; When a location is clicked on the map, its <strong>address</strong> will be filled.
   This uses <span class='hightlighted-word'>Google Map APIs - geocoder</span>.
 </pre>        
         </div>
