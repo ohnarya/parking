61c7cc55905858f0f084f992a933e3085b803701
ohnarya <ohnarya@tamu.edu>
2016-07-10 23:54:43 +0000

    deployt

---
+++ common/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+$url = parse_url(getenv("DATABASE_URL"));
+$dsn = 'pgsql:host='.$url['host'].';port='.$url['port'].';dbname='.substr($url["path"], 1);
+$username = $url["user"];
+$password = $url["pass"];
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => $dsn,
+            'username' => $username,
+            'password' =>$password,
+        ],  
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
--- common/config/main.php
+++ common/config/main.php
@@ -1,14 +1,4 @@
 <?php
-if (getenv("YII_ENV") == 'prod') {
-    $url = parse_url(getenv("DATABASE_URL"));
-    $dsn = 'pgsql:host='.$url['host'].';port='.$url['port'].';dbname='.substr($url["path"], 1);
-    $username = $url["user"];
-    $password = $url["pass"];
-} else {
-    $dsn = 'pgsql:host=localhost;dbname=parking';
-    $username = 'ubuntu';
-    $password = 'parking';
-}
 
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
@@ -22,11 +12,5 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => $dsn,
-            'username' => $username,
-            'password' =>$password,
-        ],        
     ],
 ];
